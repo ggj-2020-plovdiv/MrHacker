4cf96121d81825e174abe2d8bbc67c4b4f558bcf
Salamander333 <y.glushkov@abv.bg>
2020-02-01 00:48:27 +0200

    added basic ai

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform player;
+    public float verticalSpeed;
+    public float horizontalSpeed;
+
+    public float health;
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            player.Translate(Vector2.up * verticalSpeed, Space.World);
+        }
+        if (Input.GetKey("s"))
+        {
+            player.Translate(Vector2.down * verticalSpeed, Space.World);
+        }
+        if (Input.GetKey("a"))
+        {
+            player.Translate(Vector2.left * horizontalSpeed, Space.World);
+        }
+        if (Input.GetKey("d"))
+        {
+            player.Translate(Vector2.right * horizontalSpeed, Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerDetected.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetected : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SendMessageUpwards("ChasePlayer");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SendMessageUpwards("CheckForDirChange");
+        }
+    }
+}
