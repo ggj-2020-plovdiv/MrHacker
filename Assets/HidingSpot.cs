444b7f8636de51e1845080aa9e77ce46ffeb8f91
Salamander333 <y.glushkov@abv.bg>
2020-02-01 15:56:55 +0200

    added hiding

---
+++ Assets/HidingSpot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HidingSpot : MonoBehaviour
+{
+    public GameObject hideTextPrompt;
+
+    private GameObject player;
+
+    void Start()
+    {
+        hideTextPrompt = GameObject.FindGameObjectWithTag("HidePrompt");
+        hideTextPrompt.SetActive(false);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            hideTextPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                player.GetComponent<PlayerController>().hiding = !player.GetComponent<PlayerController>().hiding;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            hideTextPrompt.SetActive(false);
+        }
+    }
+}
